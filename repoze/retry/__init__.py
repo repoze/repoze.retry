095d4dfe6ae964a57a32ce69fd917243be54d752
Chris McDonough <chrism@plope.com>
2007-09-17 14:59:45 +0000

    We now buffer the result of a downstream application's  'start_response' call so we can retry requests which have already  called start_response without breaking the WSGI spec (the server's  start_response may only be called once unless there is an exception,  and then it needs to be called with an exc_info three-tuple, although  we're uninterested in that case here).
     Prep for 0.3.

--- repoze/retry/__init__.py
+++ repoze/retry/__init__.py
@@ -5,17 +5,34 @@ class Retry:
     def __init__(self, application, tries):
         self.application = application
         self.tries = tries
+        self.start_response_result = None
+
+    def buffer_start_response(self, *arg, **kw):
+        # we can't successfully retry if a downstream application has already
+        # called start_response, so we buffer the result and call the
+        # original start_response if we don't 
+        self.start_response_result = (arg, kw)
+
+    def call_start_response(self, start_response):
+        if self.start_response_result is not None:
+            arg, kw = self.start_response_result
+            start_response(*arg, **kw)
 
     def __call__(self, environ, start_response):
         i = 0
         while 1:
             try:
-                return self.application(environ, start_response)
+                result = self.application(environ, self.buffer_start_response)
             except ConflictError:
                 i += 1
                 if i < self.tries:
                     continue
+                self.call_start_response(start_response)
                 raise
+            else:
+                self.call_start_response(start_response)
+                return result
+                
                 
 def make_retry(app, global_conf):
     tries = int(global_conf.get('tries', 3))
--- repoze/retry/tests.py
+++ repoze/retry/tests.py
@@ -9,7 +9,10 @@ class TestRetry(unittest.TestCase):
     def _makeOne(self, *arg, **kw):
         return self._getTargetClass()(*arg, **kw)
 
-    def test_conflict_not_raised(self):
+    def setUp(self):
+        self._dummy_start_response_result = None
+
+    def test_conflict_not_raised_start_response_not_called(self):
         from ZODB.POSException import ConflictError
         application = DummyApplication(conflicts=1)
         retry = self._makeOne(application, tries=4)
@@ -17,20 +20,44 @@ class TestRetry(unittest.TestCase):
         self.assertEqual(result, ['hello'])
         self.assertEqual(application.called, 1)
 
-    def test_conflict_raised(self):
+    def test_conflict_raised_start_response_not_called(self):
         from ZODB.POSException import ConflictError
         application = DummyApplication(conflicts=5)
         retry = self._makeOne(application, tries=4)
         self.failUnlessRaises(ConflictError, retry, None, None)
         self.assertEqual(application.called, 4)
 
+    def _dummy_start_response(self, *arg):
+        self._dummy_start_response_result = arg
+
+    def test_conflict_raised_start_response_called(self):
+        from ZODB.POSException import ConflictError
+        application = DummyApplication(conflicts=5, call_start_response=True)
+        retry = self._makeOne(application, tries=4)
+        self.failUnlessRaises(ConflictError, retry, None,
+                              self._dummy_start_response)
+        self.assertEqual(application.called, 4)
+        self.assertEqual(self._dummy_start_response_result, ('200 OK', {}))
+
+    def test_conflict_not_raised_start_response_called(self):
+        from ZODB.POSException import ConflictError
+        application = DummyApplication(conflicts=1, call_start_response=True)
+        retry = self._makeOne(application, tries=4)
+        result = retry(None, self._dummy_start_response)
+        self.assertEqual(application.called, 1)
+        self.assertEqual(self._dummy_start_response_result, ('200 OK', {}))
+        self.assertEqual(result, ['hello'])
+
 class DummyApplication:
-    def __init__(self, conflicts):
+    def __init__(self, conflicts, call_start_response=False):
         self.called = 0
         self.conflicts = conflicts
+        self.call_start_response = call_start_response
 
     def __call__(self, environ, start_response):
         from ZODB.POSException import ConflictError
+        if self.call_start_response:
+            start_response('200 OK', {})
         if self.called < self.conflicts:
             self.called += 1
             raise ConflictError
