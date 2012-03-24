fb77a94393ed1ffc36ef537ee0dd51e3996698f0
Tres Seaver <tseaver@palladion.com>
2012-03-24 18:19:34 -0400

    Add 'dev' alias.

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
+testing_extras = ['nose', 'coverage']
+
 setup(name='repoze.retry',
       version=__version__,
       description='Middleware which implements a retryable exceptions',
@@ -15,6 +17,10 @@ setup(name='repoze.retry',
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
@@ -37,4 +43,7 @@ setup(name='repoze.retry',
       [paste.filter_app_factory]
       retry = repoze.retry:make_retry
       """,
-      )
+      extras_require = {
+        'testing': testing_extras,
+      }
+)
